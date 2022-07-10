7e0168cf52db5c87a1a566abafb3a0f901389da9
ManuCaetanof <manu_helena2014@hotmail.com>
2022-07-10 18:20:16 -0300

    commit3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,22 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y){
+    return x;
+  } else {
+    return y;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18){
+    return "Allowed";
+  }else {
+    return " Not Allowed";
+  }
 }
   
 function conection(status) {
@@ -19,6 +29,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if ( status === 1){
+    return "Online";
+  } else if ( status === 2){
+    return "Away";
+  }else {
+    return "Offline"
+  }
 }
 
 function saludo(idioma) {
@@ -28,7 +45,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-}
+  if (idioma === 'aleman'){
+    return "Guten Tag !";
+  }else if( idioma === "mandarin"){
+    return "Ni Hao";
+  }else if(idioma === 'ingles'){
+    return "Hello";
+  } else {
+    return "Hola"
+  }
+
+  }
+
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -67,15 +95,39 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 15 === 0){
+    return "fizzbuzz";
+  }
+  if (numero % 3 === 0){
+    return "fizz";
+  }
+  if (numero % 5 === 0){
+    return "buzz";
+}
 }
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"***
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"***
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.***
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 <0 || num2 < 0 || num3 <0){
+    return 'Hay negativos';
+  } 
+  else if ( num1 > 0 && num1 > num2 & num1 > num3){
+    return "Numero 1 es mayor y positivo";
+  } 
+  else if (num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }
+  else if ( num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  }
+  else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
